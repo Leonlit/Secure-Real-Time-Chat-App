1a832089fdf5ee0ed894ee67fbbba6310ad9ed91
LeonLit <leonlit123@gmail.com>
2022-01-14 17:21:57 +0800

    firebase connection succesfull, trying out login and registration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import "package:firebase_core/firebase_core.dart";
 import 'package:secure_real_time_chat_app/screens/wrapper.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
